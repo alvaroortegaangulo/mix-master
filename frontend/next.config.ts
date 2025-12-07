e0c53385625d73745c35ac095e6ca235c5806324
alvaroortegaangulo <alvaro.ortega.angulo@gmail.com>
2025-12-07 11:17:16 +0100

    cambio cache

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,32 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
   compress: true,
+
+  async headers() {
+    return [
+      {
+        // Aplica a TODO: páginas, API routes y estáticos de Next
+        source: "/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value:
+              "no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0",
+          },
+          {
+            key: "Pragma",
+            value: "no-cache",
+          },
+          {
+            key: "Expires",
+            value: "0",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
