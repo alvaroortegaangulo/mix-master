2d9507fa14c5646deb1c9a6dfa11eb51b3f8cf34
alvaroortegaangulo <alvaro.ortega.angulo@gmail.com>
2025-12-27 20:29:45 +0100

    dockersss 2

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,10 +1,9 @@
-import type { NextConfig } from "next";
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
-const isFastBuild = process.env.FAST_BUILD === "1";
+const isFastBuild = process.env.FAST_BUILD !== "0";
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   reactCompiler: true,
   compress: true,
   eslint: {
